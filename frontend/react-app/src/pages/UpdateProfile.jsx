a68a4185f204d6f40b1dfae3e8c976636f38979d
Diana Akhmedova <katnissgamer001@gmail.com>
2025-11-26 15:33:27 -0500

    started on update profile

---
+++ frontend/react-app/src/pages/UpdateProfile.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react"
+
+export default function UpdateProfile() {
+    
+    return (
+        <section id="profile" className="py-4">
+
+        </section>
+    )
+}
